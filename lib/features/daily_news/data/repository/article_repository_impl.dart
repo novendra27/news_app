5645b090322651b471449bc4025fdcb36ff8255c
novendra27 <adi.novendra.p@gmail.com>
2024-07-22 15:04:56 +0700

    Create data state, entities, repository, and models classes

---
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -0,0 +1,11 @@
+import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/features/daily_news/data/models/article.dart';
+import 'package:news_app/features/daily_news/domain/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+  @override
+  Future<DataState<List<ArticleModel>>> getArticles() {
+    // TODO: implement getArticles
+    throw UnimplementedError();
+  }
+}
